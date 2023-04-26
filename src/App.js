0aec382666187af36ed5492d66b8de531e7a081e
Mahipal Singh <smahipal679@gmail.com>
2023-04-25 23:06:05 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import Header from './Components/Header';
+import Home from './Components/Home';
+import Experience from "./Components/Experience";
+import Skills from "./Components/Skills";
+import Projects from "./Components/Projects";
+import './css/Home.scss';
+import './css/Experience.scss';
+import {createBrowserRouter, createRoutesFromElements, Route, Router, RouterProvider } from "react-router-dom";
+
 
 function App() {
+  const router =createBrowserRouter(  
+    createRoutesFromElements(
+          <Route path='/' element={<Header/>}>
+          <Route index element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/experience" element={<Experience />} />
+          <Route path="/skills" element={<Skills />} />
+          </Route>
+    )
+  )
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <RouterProvider router={router}/>
+    </>
   );
 }
 
